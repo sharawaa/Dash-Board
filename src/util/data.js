b65b7c25e1335287b5036e57dc649fb7f6ea8b3f
Morning <morning@admins-iMac-2.local>
2023-01-13 12:43:32 +0800

    test1

---
+++ src/util/data.js
@@ -0,0 +1,102 @@
+export const MENUS = [
+  { name: "HOME", url: "/" },
+  { name: "PRODUCT", url: "/product" },
+  { name: "LOGIN", url: "/login" },
+  { name: "USERS", url: "/users" },
+];
+
+export const users = [
+  {
+    userName: "bataa",
+    password: "qwe",
+    role: "admin",
+  },
+  {
+    userName: "suhee",
+    password: "qwe",
+    role: "admin",
+  },
+  {
+    userName: "sharva",
+    password: "qwe",
+    role: "admin",
+  },
+  {
+    userName: "suldee",
+    password: "qwe",
+    role: "admin",
+  },
+];
+
+export const DATA = [
+  {
+    description:
+      "Customize a Samsung Bespoke 3- or 4-door French door refrigerator or 4-Door Flex refrigerator with panels available in multiple colors and finishes. Plus, shop for matching appliances.",
+    spec: [{ height: "69 7/8 inches" }, { width: "35 3/4 inches" }],
+    name: "4-Door Flex",
+    id: "a4bbe2cc",
+    image:
+      "https://image-us.samsung.com/SamsungUS/home/home-appliances/refrigerators/090321/rs22t5561sr/RS22T5561SR_01_SIlver_samsung.jpg?$product-details-jpg$",
+    price: 12222,
+    stock: 12,
+    sale: 23,
+    category: "appliances",
+  },
+  {
+    name: "HP Elitebook 840",
+    spec: [
+      {
+        DisplaySize: '14"',
+        CPU: "i5-6300U 2,4 GHZ",
+        HardDrive: "1Tb SSD",
+        RAM: "16GB",
+      },
+    ],
+    id: "ea5e3b6c",
+    image: "https://i.ebayimg.com/images/g/E8wAAOSwO9Bjmd1t/s-l1600.jpg",
+    description:
+      "Used: An item that has been used previously. The item may have some signs of cosmetic wear, but is fully operational and functions as intended. This item may be a floor model or store return that has been used. See the seller’s listing for full details and description of any imperfections. ",
+    price: 180,
+    stock: 29,
+    sale: false,
+    category: "computers & tablets",
+  },
+  {
+    name: "HP Probook 430",
+    spec: [
+      {
+        DisplaySize: '13"',
+        CPU: "i5-8250U 2,4 GHZ",
+        HardDrive: "256Gb SSD",
+        RAM: "8GB",
+      },
+    ],
+    id: "bf3e8ab6",
+    image: "https://i.ebayimg.com/images/g/F2oAAOSwbxBimCXu/s-l1600.jpg",
+    description:
+      "Touch Convertible Laptop AMD. 1 year Warranty. Free and fast delivery.",
+    price: 265,
+    stock: 2,
+    sale: true,
+    category: "computers & tablets",
+  },
+  {
+    name: "Acer Spin 3",
+    spec: [
+      {
+        DisplaySize: '14"',
+        CPU: "i5-8250U 2,4 GHZ",
+        HardDrive: "512Gb SSD",
+        RAM: "8GB",
+      },
+    ],
+    id: "0bd16f1e",
+    image: "https://i.ebayimg.com/images/g/YLkAAOSwKjpjpOdr/s-l1600.jpg",
+    description:
+      "Touch Convertible Laptop AMD. 1 year Warranty. Free and fast delivery.",
+    price: 220,
+    stock: 12,
+    sale: false,
+    category: "computers & tablets",
+  },
+];
